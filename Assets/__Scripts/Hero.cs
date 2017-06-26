e2f2d03e835482811783720ba18f1c912c310a26
Michael Bandola <24bandola@gmail.com>
2017-06-26 18:44:35 -0400

    finished (part 2)

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -23,14 +23,13 @@ public class Hero : MonoBehaviour {
 	void Awake(){
 		S = this;
 		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
-		ClearWeapons ();
-		weapons [0].SetType (WeaponType.blaster);
 	}
 
 
 	// Use this for initialization
 	void Start () {
-	
+		ClearWeapons ();
+		weapons [0].SetType (WeaponType.blaster);
 	}
 	
 	// Update is called once per frame
