e2d78cc7a8bbb4df1d9c9074d3499e1f3b96e139
Michael Bandola <24bandola@gmail.com>
2017-06-25 23:32:00 -0400

    enemies

---
+++ Assets/__Scripts/Enemy_4.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_4 : Enemy {
+
+	public Vector3[] points;
+	public float timeStart;
+	public float duration = 4;
+
+	// Use this for initialization
+	void Start () {
+		points = new Vector3[2];
+		points [0] = pos;
+		points [1] = pos;
+		InitMovement ();
+	}
+
+	void InitMovement() {
+		Vector3 p1 = Vector3.zero;
+		float esp = Main.S.enemySpawnPadding;
+		Bounds cBounds = Utils.camBounds;
+		p1.x = Random.Range (cBounds.min.x + esp, cBounds.max.x - esp);
+		p1.y = Random.Range (cBounds.min.y + esp, cBounds.max.y - esp);
+		points [0] = points [1];
+		points [1] = p1;
+		timeStart = Time.time;
+	}
+
+	public override void Move() {
+		float u = (Time.time - timeStart) / duration;
+		if (u >= 1) {
+			InitMovement ();
+			u = 0;
+		}
+		u = 1 - Mathf.Pow (1 - u, 2);
+		pos = (1 - u) * points [0] + u * points [1];
+	}
+}
